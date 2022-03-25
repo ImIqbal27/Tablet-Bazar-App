2f627ec70dcc8ddd0d5b4ffcf079962e771266ac
unknown <iamhasan9501@gmail.com>
2022-03-26 00:43:16 +0600

    first commit

---
+++ src/components/Header.js/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h2 className='headline'>Tablet Bazar</h2>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/about">About</a>
+            </div>
+
+        </nav>
+    );
+};
+
+export default Header;
