557fd2f317c7c373bdb6da884ece62d11ef1516f
unknown <iamhasan9501@gmail.com>
2022-03-26 02:21:38 +0600

    img optimize

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,16 @@ import './Product.css';
 
 const Product = (props) => {
     // console.log(props.product)
-    const { id, name, price, img } = props.product;
+    const { name, price, img, brand } = props.product;
     return (
         <div className='product'>
-            <img src={img}></img>
-            <p className='product-name'>{name.toUpperCase()}</p>
+            <img src={img} alt=""></img>
+            <div className='product-info'>
+                <p className='product-name'>{name.toUpperCase()}</p>
+                <p>Price:{price}</p>
+                <p>Brand:{brand.toUpperCase()}</p>
+            </div>
+
         </div>
     );
 };
