fdcd662d5f4e1c2c3795ad1132d39a45e2a065f5
unknown <iamhasan9501@gmail.com>
2022-03-26 16:55:56 +0600

    item added in the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,15 +4,25 @@ const Cart = ({ cart }) => {
     // console.log(cart);
     let cartList = [];
     for (const product of cart) {
-        cartList.push(product.name)
+        if (cartList.length < 4) {
+            cartList.push(product.name)
+        }
+        else {
+            // alert("Ops, You cant add more than 4 items !")
+        }
     }
-    // cartList.map()
-    // console.log(cartList);
+
     return (
         <div className='cart'>
-            <h2>Order Summary</h2>
-            <p>Selected Items:  {cart.length}</p>
-            <p> {cartList}</p>
+            <div>
+                <h2>Order Summary</h2>
+                <p>Selected Items:  {cart.length}</p>
+                <p> {cartList}</p>
+            </div>
+            <div>
+                <button className='btn-choose-1'>CHOOSE 1 FOR ME</button>  <br />
+                <button className='btn-reset'>RESET</button>
+            </div>
 
 
         </div>
